0099714bd0c8c558ed602705e4d9c4677ee0ac0b
Jdepetr1 P <49722708+jdepetr1@users.noreply.github.com>
2022-02-17 15:13:39 +0000

    Added Functions for drawing polygons and clearing them

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import turtle               #1. import modules
 import random
 
+def draw_polygon(num_sides = 3, side_len = 40):
+  turn_angle = 360/num_sides
+
+  for i in range(num_sides):
+    leonardo.forward(side_len)
+    leonardo.left(turn_angle)
+
+def clear_turtle_drawings(my_turtle = None):
+  my_turtle.clear()
+
 #Part A
 window = turtle.Screen()       # 2.  Create a screen
 window.bgcolor('lightblue')
@@ -38,56 +48,36 @@ leonardo.goto(-100,-20)
 leonardo.down()
 side_len = 90
 num_sides = 3
-turn_angle = 360/num_sides
 
-for i in range(num_sides):
-  leonardo.forward(side_len)
-  leonardo.left(turn_angle)
-
-leonardo.clear()
+draw_polygon(num_sides, side_len)
+clear_turtle_drawings(leonardo)
 
 #Square (4)
 side_len = 80
 num_sides = 4
-turn_angle = 360/num_sides
 
-for i in range(num_sides):
-  leonardo.forward(side_len)
-  leonardo.left(turn_angle)
-
-leonardo.clear()
+draw_polygon(num_sides, side_len)
+clear_turtle_drawings(leonardo)
 
 #Hexagon (6)
 side_len = 75
 num_sides = 6
-turn_angle = 360/num_sides
 
-for i in range(num_sides):
-  leonardo.forward(side_len)
-  leonardo.left(turn_angle)
-
-leonardo.clear()
+draw_polygon(num_sides, side_len)
+clear_turtle_drawings(leonardo)
 
 #Nonagon (9)
 side_len = 50
 num_sides = 9
-turn_angle = 360/num_sides
 
-for i in range(num_sides):
-  leonardo.forward(side_len)
-  leonardo.left(turn_angle)
-
-leonardo.clear()
+draw_polygon(num_sides, side_len)
+clear_turtle_drawings(leonardo)
 
 #Dodecagon (12)
 side_len = 40
 num_sides = 12
-turn_angle = 360/num_sides
 
-for i in range(num_sides):
-  leonardo.forward(side_len)
-  leonardo.left(turn_angle)
-
-leonardo.clear()
+draw_polygon(num_sides, side_len)
+clear_turtle_drawings(leonardo)
 
 window.exitonclick()
